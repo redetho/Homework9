373577cf0c38c468e7f7ff64dfbc5c9571af71f4
Thomas Redekop <redetho@gmail.com>
2021-10-20 22:03:42 -0300

    branch abstratos

---
+++ atividade/Entities/ContaPoupança.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace atividade.Entities
+{
+    class ContaPoupança : Conta
+    {
+        //declaração de variáveis e encapsulamento
+        public double TaxaJuros { get; set;}
+        //construtor padrão
+        public ContaPoupança() { }
+        //construtor com parâmetros
+        //valor dos juros:
+        public ContaPoupança(int numero, string titular, double saldo, double taxaJuros)
+            : base(numero, titular, saldo)
+        {
+            TaxaJuros = taxaJuros;
+        }
+        //atualiza o saldo de acordo com os juros
+        public void atualizacaoSaldo()
+        {
+            Saldo += Saldo * TaxaJuros;
+        }
+        //sobreposição
+
+    }
+    
+}
--- atividade/Entities/Program.cs
+++ atividade/Entities/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 
 namespace atividade.Entities
@@ -8,8 +10,28 @@ namespace atividade.Entities
         //apresenta o saldo da conta.
         static void Main(string[] args)
         {
-            var ContaJuridicaConta = new ContaJuridica(8010, "LaraCroft", 100.0, 500.0);
-            Console.WriteLine(ContaJuridicaConta.Saldo);
+            //contas, numero, nome, etc.
+            List<Conta> list = new List<Conta>();
+            list.Add(new ContaPoupança(1001, "Terry", 500.0, 0.01));
+            list.Add(new ContaJuridica(1002,"Andy", 500.0, 400.0));
+            list.Add(new ContaPoupança(1003,"Joe", 500, 0.01));
+            list.Add(new ContaJuridica(1004,"Mae", 500, 400.0));
+            double soma = 0.0;
+            foreach(Conta acc in list)
+            {
+                soma += acc.Saldo;
+            }
+            //calcula o saldo de todas contas
+            Console.WriteLine("Total do saldo de todas as contas: " +soma.ToString("F2", CultureInfo.InvariantCulture));
+            foreach(Conta acc in list)
+            {
+                acc.Saque(10.0);
+            }
+            //saque e atualização das contas
+            foreach(Conta acc in list)
+            {
+                Console.WriteLine("atualização do saldo das contas" +acc.Numero + ":" + acc.Saldo.ToString("F2", CultureInfo.InvariantCulture));
+            }
         }
     }
 }
